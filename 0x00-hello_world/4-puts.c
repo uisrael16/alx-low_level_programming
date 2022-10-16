4ed65a25cd34b686462d2ca1f63caecb705ba324
Uisrael16 <unathinkomo16@gmail.com>
2022-10-16 13:25:03 -0700

    created 4-puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * * Description: prints "\"Programming is like building a multilingual puzzle"
  * * Return: 0
  *
- * */
+*/
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
