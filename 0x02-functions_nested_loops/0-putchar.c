3a88364220c4afc8daa9be3bfb01fa40b43361e9
Uisrael16 <unathinkomo16@gmail.com>
2022-10-19 14:02:03 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include "holberton.h"
 
 /**
- *  * main - Prints "Holberton" followed by a new line.
+ *  * main - Print "Holberton"
  *   *
- *    * Return: Always 0.
+ *    * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	int i = 0;
+	char name_of_school[] = "Holberton";
 
+	while (i < 9)
+	{
+		_putchar(name_of_school[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
