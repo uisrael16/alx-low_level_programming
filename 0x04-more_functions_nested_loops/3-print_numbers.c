1c664300350ae33e0412cf2ffc537596a2a34e29
Uisrael16 <unathinkomo16@gmail.com>
2022-10-22 14:34:37 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,16 +5,12 @@
  *   *
  *    * Return: 0-9 followed by newline
  */
+
 void print_numbers(void)
 {
 	int i;
-	i = 0;
 
-	while( i<= 9)
-	{
+	for (i = 0; i <= 9; i++)
 		_putchar(i + '0');
-		i++;
-	}
-
 	_putchar('\n');
 }
