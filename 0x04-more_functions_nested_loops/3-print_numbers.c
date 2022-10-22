3251ed5000e791a361d3f8dc43b268cd6ce493e1
Uisrael16 <unathinkomo16@gmail.com>
2022-10-22 14:31:03 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *  * print_numbers - a function that prints the numbers, from 0 to 9
+ *   *
+ *    * Return: 0-9 followed by newline
+ */
+void print_numbers(void)
+{
+	int i;
+	i = 0;
+
+	while( i<= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+
+	_putchar('\n');
+}
