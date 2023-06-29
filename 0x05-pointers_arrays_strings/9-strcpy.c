25441f866c2eeaa1d1caa8d192961f60f6fb0135
uisrael16 <uisrael@student.wethinkcode.co.za>
2023-06-29 13:47:59 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "coding.h"
+
+/**
+ * _strcpy - function with two arguments
+ * @dest: 1st argument char type pointer
+ * @src: 2nd argument char type pointer
+ *
+ * Description: copies the string to an array
+ * Return: na
+ */
+char *_strcpy(char *dest, char *src)
+{
+	size_t	counter;
+	size_t	len;
+
+	len = ft_strlen(src);
+	counter = 0;
+	while (!src)
+	{
+		return (NULL);
+	}
+	while (counter < len)
+	{
+		dst[counter] = src[counter];
+		counter++;
+	}
+	dst[counter] = '\0';
+	return (dst);
+}
