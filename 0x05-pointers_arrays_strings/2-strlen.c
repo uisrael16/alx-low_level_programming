98bc00e3a38c6699a2eabc8c14f231decc93a58a
uisrael16 <uisrael@student.wethinkcode.co.za>
2023-06-29 04:15:56 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - function with 1 argument
+ * @s: char type pointer
+ *
+ * Description: strlen similiar function to return length
+ * Return: string
+ */
+int _strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
