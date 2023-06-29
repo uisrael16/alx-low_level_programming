5efd42d5f0ebb4a30840864073dddb3dcf0035bf
uisrael16 <uisrael@student.wethinkcode.co.za>
2023-06-29 04:53:56 -0700

    4

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "maini.h"
+
+/**
+ * rev_string - function with one argument
+ * @s: char type argument
+ *
+ * Description: reversing a string
+ * Return: na
+ */
+void rev_string(char *s)
+{
+	int i, j, tmp, len;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	len = i;
+	i--;
+	while (j < len / 2)
+	{
+		tmp = s[j];
+		s[j] = s[i];
+		s[i] = tmp;
+		i--;
+		j++;
+	}
+}
