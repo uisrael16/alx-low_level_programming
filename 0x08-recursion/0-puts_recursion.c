8109ed18624f146069b1022c5b92731cfc3709eb
uisrael16 <uisrael@student.wethinkcode.co.za>
2023-07-06 13:06:07 -0700

    hi

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,10 @@
 
 void _puts_recursion(char *s)
 {
-    if (!s[0])
-    {
-        _putchar('\n');
-    }
-    else
-        _putchar(s[0]);
-        _puts_recursion(s + 1);
+   if (*s == '0')
+    putchar('\n');
+else
+    putchar(*s);
+    _puts_recursion((s + 1));
+}
 }
