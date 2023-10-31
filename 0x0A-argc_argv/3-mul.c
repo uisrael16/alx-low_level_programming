4587b99a0dc1baceba8ac7ff1fecf103a8024a78
Unathi <unathinkomo16@gmail.com>
2023-10-31 13:17:38 -0700

    argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - program that multiplies two numbers.
+ * @argc: number of arguments
+ * @argv: array with the arguments
+ *
+ * Return: always 0
+ **/
+
 int main(int argc, char *argv[])
 {
     if (argc != 3) {
