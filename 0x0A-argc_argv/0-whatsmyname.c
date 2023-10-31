41fa1eae9ad1edbe8a3a4d18ac00685a54c55b25
Unathi <unathinkomo16@gmail.com>
2023-10-31 12:46:53 -0700

    argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: Always returns 0
+ **/
+int main(int argc, char *argv[])
+{
+    (void)argc;
+
+    printf("%s\n", argv[0]);
+
+    return 0;
+}
+
