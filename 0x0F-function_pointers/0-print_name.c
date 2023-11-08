bfed6391cc4318bc20599e488343b941acd59757
Unathi <unathinkomo16@gmail.com>
2023-11-08 13:43:47 -0800

    08/11

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
+void	print_name(char *name, void (*f)(char *))
+{
+  
+    if (f != NULL)
+    {
+        f(name);
+    }
+}
+
