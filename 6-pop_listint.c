6d24f6b60731980aa07278f18ee2c598bd4b5585
Unathi Nkomo <50695696+uisrael16@users.noreply.github.com>
2023-11-21 23:51:19 +0200

    Add files via upload

---
+++ 6-pop_listint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * pop_listint - deletes the head node of
+ * a linked list
+ * @head: head of a list.
+ *
+ * Return: head node's data.
+ */
+int pop_listint(listint_t **head)
+{
+	int node;
+	listint_t *h;
+	listint_t *curr;
+
+	if (*head == NULL)
+		return (0);
+
+	curr = *head;
+
+	hnode = curr->n;
+
+	h = curr->next;
+
+	free(curr);
+
+	*head = h;
+
+	return (node);
+}
