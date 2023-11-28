6795940fbd3edec8f804697ceb232d8a241fe990
uisrael16 <uanthinkomo16@gmail.com>
2023-11-28 07:31:09 -0800

    manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,11 @@
-#include "main.h"
 
-/*Check if the current character is not a valid binary digit
+/**
+ * Check if the current character is not a valid binary digit
  *Invalid character, return 0
  *Convert the character to the corresponding binary digit
  */
 
-unsigned int    binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result;
 	if (b == NULL)
@@ -15,7 +15,8 @@ unsigned int    binary_to_uint(const char *b)
 
 	result = 0;
 
-	while (*b != '\0') {
+	while (*b != '\0')
+	{
 		if (*b != '0' && *b != '1')
 		{
 			return (0);
