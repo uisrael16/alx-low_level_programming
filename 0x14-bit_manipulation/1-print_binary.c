cbe192bf3a350b8bff775485267b4d1c0733d856
uisrael16 <uanthinkomo16@gmail.com>
2023-12-05 01:20:06 -0800

    commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		putchar((n & 1) + '0');
+	}
+	else
+	{
+		putchar('0');
+	}
+}
